195436c70b4a6c91bbbede5220e0b527c155cdf1
Henrique Pasquali <hspasquali@gmail.com>
2026-05-14 10:13:21 -0300

    Adiciona card comunicado em massa

--- src/app/crm/campanhas/page.tsx
+++ src/app/crm/campanhas/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
+import ComunicadoMassaCard from "../../../components/crm/ComunicadoMassaCard";
 import {
   Activity,
   Brain,
